767bc16a4b75ead4532a23673f4b33756ac96681
Matt Swanson <mdswanson@sep.com>
2011-08-16 21:44:30 -0400

    serve static files

--- dasher.js
+++ dasher.js
@@ -1,8 +1,15 @@
-var app = require('express').createServer()
-    , io = require('socket.io').listen(app);
+var express = require('express')
+    , io = require('socket.io');
 
-app.listen(80);
+app = express.createServer();
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.send('sup');
 });
+
+app.listen(80);
+io.listen(app);
+
+console.log('Server started on port %s', app.address().port);
